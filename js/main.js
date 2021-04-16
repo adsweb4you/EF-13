918cd9d7c3418f47cd5216a98fd574cd74de11d2
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-04-16 20:17:36 +0400

    last

---
+++ js/main.js
@@ -0,0 +1,69 @@
+var z,x,m,l;
+
+const MAXFILE_SIZE = 5;
+const MAXFILE_SIZE1 = 5;
+
+z = 0;
+x = "x";
+m = "m";
+l = "l";
+
+z = 5;
+
+var names = 25;
+var x = 15;
+var y = names - x;
+
+const virable = document.getElementById("test").value;
+
+ 
+let strings = "<p> დასახელება </p>";
+let numb = 50;
+let array = [1, 5, "name"];
+let obj = {names:"sds"};
+let bool = true;
+let bol = false;
+let nulls = null;
+let und = undefined;
+let funs = function na(){}
+
+
+// console.log( Array.isArray(obj) );
+
+
+let divs = document.getElementById("outs");
+
+divs.innerHTML = strings;
+//divs.innerText = strings;
+
+
+
+// console.log(numb1)
+
+// ფუნქციის შექმნა
+function test(name  , lastname , age  ){
+ return name  +  " " +   lastname + " არის " + age + " წლის ";
+}
+
+// ფუნქციის გამოძახება
+
+let gio = test("გიორგი", "გიორგობიანი", 25);
+
+let testi = test("ნიკა", "ნიკოლაძე", 27);
+
+
+// let admin = test();
+
+// console.log(gio);
+// console.log(testi);
+// console.log(admin);
+
+let img = 'img/item.png';
+let clas = 'img-fluid';
+
+//let html   = "<img src='" + img  + "'  class='"+ clas +"'>";
+
+let html   =  `<img src="${img}" class="${clas}">`;
+
+
+document.getElementById("cards").innerHTML = html;
