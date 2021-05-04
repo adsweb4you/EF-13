39f5b13f6dca223b19fc566a8eeb986d4d92f0d1
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-05-04 22:02:55 +0400

    date

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+ 
+
+
 var z,x,m,l;
 
 const MAXFILE_SIZE = 5;
@@ -265,20 +269,186 @@ function calc (els){
 let filteredArray  = Products.filter(calc);
 
 
-let outs = document.getElementById("htmls");
+// let outs = document.getElementById("htmls");
 
-filteredArray.forEach(forprod);
+// filteredArray.forEach(forprod);
 
-function forprod(elements){
-   let prod =  `<div class="col-lg-3 mb-4"> <div class="card">
-   <img src="${elements.img}" class="card-img-top" alt="...">
-   <div class="card-body">
-     <h5 class="card-title">${elements.name}</h5>
-     <p class="card-text">${elements.descr}</p> 
-     <span class="btn btn-primary">${elements.price} ₾</span>
-   </div>
-  </div>
- </div>`;
+// function forprod(elements){
+//    let prod =  `<div class="col-lg-3 mb-4"> <div class="card">
+//    <img src="${elements.img}" class="card-img-top" alt="...">
+//    <div class="card-body">
+//      <h5 class="card-title">${elements.name}</h5>
+//      <p class="card-text">${elements.descr}</p> 
+//      <span class="btn btn-primary">${elements.price} ₾</span>
+//    </div>
+//   </div>
+//  </div>`;
 
- outs.innerHTML += prod;
+//  outs.innerHTML += prod;
+// }
+
+ 
+
+ 
+
+
+let list =[
+    {name: "გიორგი", photo:"pic.png", age: 20, reg: "2020/10/22", faculty: "პროგრამირება", attendance:85, stipendium: false },
+    {name: "გიორგი", photo:"pic.png", age: 19, reg: "2020/10/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "გიორგი", photo:"pic.png", age: 22, reg: "2020/10/22", faculty: "პროგრამირება", attendance:85, stipendium: false },
+    {name: "ლევან", photo:"pic.png", age: 20, reg: "2020/09/22", faculty: "მეღვინეობა", attendance:90, stipendium: false },
+    {name: "ლაშა", photo:"pic.png", age: 12, reg: "2020/09/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "ანი", photo:"pic.png", age: 22, reg: "2020/09/22", faculty: "პროგრამირება", attendance:85, stipendium: false },
+    {name: "ლუკა", photo:"pic.png", age: 20, reg: "2020/09/22", faculty: "პროგრამირება", attendance:90, stipendium: true },
+    {name: "ალექსანდრე", photo:"pic.png", age: 21, reg: "2020/09/22", faculty: "პროგრამირება", attendance:100, stipendium: false },
+    {name: "საბა", photo:"pic.png", age: 19, reg: "2020/09/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "მარიამ", photo:"pic.png", age: 22, reg: "2020/09/22", faculty: "ფსიქოლოგია", attendance:100, stipendium: false },
+    {name: "თამარ", photo:"pic.png", age: 21, reg: "2020/09/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "ნინო", photo:"pic.png", age: 21, reg: "2020/09/22", faculty: "პროგრამირება", attendance:100, stipendium: false },
+    {name: "კესანე", photo:"pic.png", age: 22, reg: "2020/07/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "ლევან", photo:"pic.png", age: 19, reg: "2020/07/22", faculty: "პროგრამირება", attendance:90, stipendium: false },
+    {name: "გიორგი", photo:"pic.png", age: 20, reg: "2020/11/22", faculty: "ფსიქოლოგია", attendance:90, stipendium: true },
+    {name: "გიორგი", photo:"pic.png", age: 21, reg: "2020/12/22", faculty: "პროგრამირება", attendance:100, stipendium: true },
+    {name: "გიორგი", photo:"pic.png", age: 21, reg: "2020/12/22", faculty: "ფსიქოლოგია", attendance:90, stipendium: false },
+    {name: "გიორგი", photo:"pic.png", age: 20, reg: "2020/13/22", faculty: "პროგრამირება", attendance:85, stipendium: true },
+    {name: "გიორგი", photo:"pic.png", age: 22, reg: "2020/08/22", faculty: "პროგრამირება", attendance:100, stipendium: false },
+    {name: "გიორგი", photo:"pic.png", age: 12, reg: "2020/07/22", faculty: "ფსიქოლოგია", attendance:85, stipendium: false },
+    {name: "გიორგი", photo:"pic.png", age: 21, reg: "2020/06/22", faculty: "მეღვინეობა", attendance:100, stipendium: true },
+    {name: "კესანე", photo:"pic.png", age: 21, reg: "2020/09/22", faculty: "პროგრამირება", attendance:100, stipendium: true },
+    {name: "ლაშა", photo:"pic.png", age: 22, reg: "2020/07/22", faculty: "ფსიქოლოგია", attendance:90, stipendium: true },
+    {name: "ანი", photo:"pic.png", age: 21, reg: "2020/07/22", faculty: "პროგრამირება", attendance:85, stipendium: false },
+    {name: "ლუკა", photo:"pic.png", age: 20, reg: "2020/07/22", faculty: "პროგრამირება", attendance:85, stipendium: false },
+    {name: "ალექსანდრე", photo:"pic.png", age: 21, reg: "2020/07/22", faculty: "პროგრამირება", attendance:90, stipendium: true },
+    {name: "საბა", photo:"pic.png", age: 22, reg: "2020/07/22", faculty: "მეღვინეობა", attendance:85, stipendium: false },
+    {name: "მარიამ", photo:"pic.png", age: 22, reg: "2020/07/22", faculty: "პროგრამირება", attendance:90, stipendium: true },
+    {name: "თამარ", photo:"pic.png", age: 20, reg: "2020/07/22", faculty: "ფსიქოლოგია", attendance:90, stipendium: false },
+    {name: "ნინო", photo:"pic.png", age: 19, reg: "2020/07/22", faculty: "პროგრამირება", attendance:100, stipendium: true }
+
+];
+
+let finalList  = document.getElementById("finalList");
+
+ 
+
+
+    function selectprof(val){
+
+        finalList.innerHTML = ''
+
+    if(val == 1){
+        var prof  = "პროგრამირება";
+    }else if (val == 2){
+        var prof  = "მეღვინეობა";
+    }else{
+        var prof  = "ფსიქოლოგია";
+    }
+
+        let ages = list.filter(findage)
+ 
+        function findage(lis){
+            return lis.faculty == prof;
+        }
+
+        ages.forEach(showList);
+
+    }
+
+    
+    function finds(val){
+
+        finalList.innerHTML = ''
+ 
+
+        let ages = list.filter(findage)
+ 
+        function findage(lis){
+            return !lis.name.search(val);
+        }
+
+        ages.forEach(showList);
+
+    }
+ 
+ 
+
+    function showList(element){
+   
+        let persona = `
+        <div class="col-md-4 card-group">
+        <div class="card mb-3 shadow ">
+            <div class="row g-0">
+                <div class="col-md-4 d-flex align-items-center">
+                    <img class="card-img" src="${element.photo}" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body  ">
+                        <h4 class="card-title">${element.name}</h4>
+                        <p class="card-text"> ასაკი: ${element.age}</p>
+                        <p class="card-text">${element.faculty}</p>
+                        <span class="badge bg-light text-dark p-1 mb-3">${element.reg}</span>
+                        <div class="card-footer p-0" >
+                      </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    
+    finalList.innerHTML += persona;
+    }
+
+
+
+
+//   d.setFullYear(d.getFullYear() + 2);
+//   d.setMonth(d.getMonth() + 2);
+ //console.log(d.getFullYear(), d.getMonth() + 1 , d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds())
+
+//console.log(d.getMonth() + 1);
+
+
+let timer = document.getElementById("timer");
+
+
+let tim = 25;
+
+
+
+function getTimeRemaining(){
+    const total = Date.parse("2022/05/10") - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+   
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+   
+    };
+  }
+
+
+//   setInterval(function(){
+//     let time2 = getTimeRemaining();
+//     console.log( time2.days ,time2.hours  , time2.minutes,time2.seconds);
+// }, 1000)
+
+
+  let tests = 6;
+  let tests2 = 9;
+  let tests4 = false;
+if(tests == 6 &&  tests2 >=  8 &&  tests4){
+    console.log("მართალი");
+}else if(12 == 13){
+    console.log("მართალი/მცდარი");
 }
+else{
+    console.log("მცდარი");
+}
+
+ 
