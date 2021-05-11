7b5387b9c7305ad2db76099fe87ee8c38d85c961
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-05-11 21:08:14 +0400

    class dom

--- js/dom.js
+++ js/dom.js
@@ -42,3 +42,41 @@ let animal = document.querySelectorAll("h1.animal");
      }
    
  }
+
+
+// function  submitform(e) {
+//    e.preventDefault();
+
+//    let wid1 = document.getElementById("val1").value;
+//    let wid2 = document.getElementById("val2").value;
+//    let wid3 = document.getElementById("val3").value;
+
+//    let price = 50;
+
+//    document.getElementById("outp").innerText = (wid1 * wid2 * wid3) * price;
+// }
+
+let menu = document.querySelector(".nav-link.active");
+let side = document.querySelector(".sidenav");
+
+let close = document.querySelector(".btn-danger.close");
+
+menu.addEventListener("click", function (event) {
+   side.classList.toggle("show");
+});
+
+
+close.addEventListener("click", function (event) {
+   side.classList.remove("show");
+});
+ 
+document.addEventListener("click", function (event) {
+console.log(event.target.classList);
+ if(event.target.classList[0] == "sidenav" || event.target.classList[0] ==  "nav-link" || event.target.classList[0] == "uls"){
+   
+ }else{
+    side.classList.remove("show");
+    
+ }
+
+  });
