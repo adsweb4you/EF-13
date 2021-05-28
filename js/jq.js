e1f05dfbfd5543127f12c9b8c028d859834bd94a
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-05-28 18:52:32 +0400

    animate

---
+++ js/jq.js
@@ -0,0 +1,81 @@
+$(function () {
+    
+// $(".btn").click(function (e) { 
+//     e.preventDefault();
+
+    
+//     $(".tests").hide(5000, function () {
+//         alert("ანიმაცია დამთავრდა")
+//     });
+// });
+
+
+// $(".show").click(function (e) { 
+//     e.preventDefault();
+//     $(".tests").show("left")
+// });
+
+
+// $(".showhide").click(function (e) { 
+//     e.preventDefault();
+//     $(".tests").slideToggle()
+// });
+
+
+// let maxlen = 50;
+
+// $("#mytext").on("input",function ( ) { 
+ 
+// let val = Number($(this).val().length) + 400;
+// let valorign = $(this).val().length;
+
+// if (valorign <= 600) {
+   
+//  $(".symbol").text(valorign);
+//     $(".circle").attr("style", "stroke-dashoffset:" + (val + 17));
+
+//  if(valorign > 500 && valorign < 550){
+//      $(".circle").attr("stroke", "yellow");
+//  }else if(valorign > 550){
+//      $(".circle").attr("stroke", "red");
+//  }
+ 
+// }else{
+//     $("#mytext").prop('disabled', true)
+// }
+
+// });
+
+ //    
+
+
+$(".animate").click(function (e) { 
+     $("form.w-100").animate({
+         "height":"0",
+ 
+
+     }, 2000, function () {
+        $("form.w-100").css({"overflow":"hidden"});
+        $(".animate").animate({
+            "opacity":0,
+        }, 1000,  function () {
+            $("#tetx").animate({
+              
+                "top": "15%",
+ 
+            }, 5000, function () {
+                $("#an").removeClass("d-flex").addClass("d-none")
+            })
+        })
+     })
+    
+});
+
+
+$(".stop").click(function (e) { 
+     console.log("asfdsf");
+    $("form.w-100").stop();
+});
+
+});
+
